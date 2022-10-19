bfa646b48ba60d0f9b5b9e3d34166e81b0e193eb
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-10-19 19:06:09 -0300

    fazendo o requesito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('testando pagina não encontrada', () => {
+  test('este se a página contém um título h2com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const titulo = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+    expect(titulo).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagem = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(imagem.src).toContain(url);
+  });
+});
